7d9331fbf1a5e9f0007f18e01d46cacb7fa78394
paragrghoshal <parag.qapro@gmail.com>
2023-09-25 21:56:53 -0400

    initial code

---
+++ src/test/java/com/parag/AppTest.java
@@ -0,0 +1,21 @@
+package com.parag;
+
+import org.testng.annotations.Test;
+
+
+import org.testng.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertEquals("Parag", "Parag");
+    }
+}
